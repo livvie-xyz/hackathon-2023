9fcad7fcfc9748864045f795f47c768339f49d61
livvie <omedeir1@swarthmore.edu>
2023-11-11 12:49:53 -0500

    database work and random button thing

--- public/app.js
+++ public/app.js
@@ -1,4 +1,21 @@
-function generateRecipe() {
+// This document handles the frontend logic for the app!
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Fetch possible ingredients from the server
+  fetch('/api/ingredients')
+    .then(response => response.json())
+    .then(data => {
+      // Initialize the selectize plugin on the ingredients input field
+      $('#ingredients').selectize({
+        options: data,
+        create: true,
+        placeholder: 'Type to search ingredients',
+      });
+    });
+
+  // Attach the generateRecipe function to the click event of the button
+  document.getElementById('generateButton').addEventListener('click', () => {
+    console.log('Generating recipe...');
     window.location.href = '/generate';
-  }
-  
+  });
+});
--- server.js
+++ server.js
@@ -1,22 +1,42 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-
-// Your backend code goes here
+app.use(bodyParser.json());
 
 // Generate a unique identifier for each recipe page
 function generateUniqueId() {
   return Math.random().toString(36).substr(2, 9);
 }
 
+// MongoDB connection URI (replace it with your MongoDB connection string)
+const mongoURI = 'mongodb://localhost:27017/recipe-generator';
+
+// Create a MongoDB client and connect to the database
+const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database', error);
+  }
+}
+
+connectToDatabase();
+
+// Your backend code goes here
+
 // Serve the recipe page
 app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-  
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 app.get('/generate', (req, res) => {
   const uniqueId = generateUniqueId();
@@ -28,6 +48,25 @@ app.get('/recipe/:id', (req, res) => {
   const recipeId = req.params.id;
   res.sendFile(path.join(__dirname, 'public', 'recipe.html'));
 });
+
+// API endpoint to get possible ingredients
+app.get('/api/ingredients', async (req, res) => {
+  const ingredients = await getIngredientsFromDatabase();
+  res.json(ingredients);
+});
+
+// Generate a unique identifier for each recipe page
+function generateUniqueId() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+// Retrieve possible ingredients from the database
+async function getIngredientsFromDatabase() {
+  const db = client.db();
+  const collection = db.collection('ingredients');
+  const result = await collection.distinct('name');
+  return result;
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
